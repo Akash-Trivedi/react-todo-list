0a40a54d4762e43b8efe40d9fff3ca5c948bc907
Akash-Trivedi <sunnytrivedi321@gmail.com>
2022-01-29 13:10:52 +0530

    footer completed and header home removed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import Header from './MyComponents/Header';
-import {Footer} from './MyComponents/Footer';
+import Footer from './MyComponents/Footer';
 import Todos from './MyComponents/Todos';
 import NewTodo from './MyComponents/NewTodo';
 
@@ -10,6 +10,7 @@ function App() {
     <Header title="Todo List"/>
     <NewTodo />
     <Todos />
+    <Footer />
     </>
   );
 }
--- src/MyComponents/Footer.js
+++ src/MyComponents/Footer.js
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from 'react';
+import {Github} from 'react-bootstrap-icons'
 
-export const Footer = () => {
+export default function Footer(){
   return (
-    <div className="container bg-dark sm">
-      <p>Copyright &copy;</p>
+    <div className="container">
+      <footer className="bg-light text-center text-lg-start">
+      <div className="text-center p-3" style={{"backgroundColor": "rgba(0, 0, 0, 0.2)"}}>
+        {`© 2022 Copyright: `}
+        <a className="text-dark" href="https://github.com/Akash-Trivedi"><Github /></a>
+      </div>
+    </footer>
     </div>
   )
 }
